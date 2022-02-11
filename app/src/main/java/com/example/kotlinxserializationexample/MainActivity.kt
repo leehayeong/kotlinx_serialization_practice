2b73eee2c0d8f58b25d36f161630315b5740f6fc
leehayeong <lhaayyy@naver.com>
2022-02-11 15:51:38 +0900

    [3] json string -> data class 생성 예제

--- app/src/main/java/com/example/kotlinxserializationexample/MainActivity.kt
+++ app/src/main/java/com/example/kotlinxserializationexample/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import com.example.kotlinxserializationexample.model.Dept
 import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
@@ -15,12 +16,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         makeDeptToJson()
+        makeJsonToDept()
     }
 
     private fun makeDeptToJson() {
         val dept = Dept(1, "Marketing", "USA/Seattle")  // data class 생성
         val deptJson = Json.encodeToString(dept)    // data class -> Json 으로 변환
         Log.d(TAG, deptJson)    // D/MainActivity: {"no":1,"name":"Marketing","location":"USA/Seattle"}
+    }
+
+    private fun makeJsonToDept(){
+        val dept = Dept(1, "Marketing", "USA/Seattle")
+        val deptJson = Json.encodeToString(dept)
+        val deptFromJson = Json.decodeFromString<Dept>(deptJson) // json string -> data class 로 변환
+        Log.d(TAG, deptFromJson.toString()) // Dept(no=1, name=Marketing, location=USA/Seattle)
     }
 
     companion object {
